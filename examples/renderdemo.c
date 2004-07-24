4b9a1f1f9d9bf8cff161f9a57b78a9f5fe950b94
Morten Welinder <terra@gnome.org>
2004-07-24 02:25:48 +0000

    Fix C99ism. I am clearly the last man stuck in the dark age of C.
    2004-07-23  Morten Welinder  <terra@gnome.org>
    
    	* examples/renderdemo.c (parse_ellipsis): Fix C99ism.  I am clearly
    	the last man stuck in the dark age of C.

--- examples/renderdemo.c
+++ examples/renderdemo.c
@@ -306,11 +306,12 @@ parse_ellipsis (ArgContext *arg_context,
 		gpointer    data)
 {
   static GEnumClass *class = NULL;
+  GEnumValue *value;
 
   if (!class)
     class = g_type_class_ref (PANGO_TYPE_ELLIPSIZE_MODE);
   
-  GEnumValue *value = g_enum_get_value_by_nick (class, arg);
+  value = g_enum_get_value_by_nick (class, arg);
   if (!value)
     fail ("--ellipsize option must be one of none/start/middle/end");
 
