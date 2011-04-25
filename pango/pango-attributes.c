db3ac6083dcd3fe6c9f31c70d314f8a572545057
Alberto Ruiz <aruiz@gnome.org>
2011-04-25 20:52:48 +0100

    Fixing array introspection annotation for pango_attr_iterator_get_font

--- pango/pango-attributes.c
+++ pango/pango-attributes.c
@@ -1784,7 +1784,7 @@ pango_attr_iterator_get (PangoAttrIterator *iterator,
  *        <literal>pango_font_description_set_family (desc, pango_font_description_get_family (desc))</literal>.
  * @language: (allow-none): if non-%NULL, location to store language tag for item, or %NULL
  *            if none is found.
- * @extra_attrs: (allow-none) (element type Pango.Attribute) (transfer full): if non-%NULL,
+ * @extra_attrs: (allow-none) (element-type Pango.Attribute) (transfer full): if non-%NULL,
  *           location in which to store a list of non-font
  *           attributes at the the current position; only the highest priority
  *           value of each attribute will be added to this list. In order
