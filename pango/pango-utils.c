50d7ad6f2073742c7b0423f578171b380b8196f5
Alberto Ruiz <aruiz@gnome.org>
2011-04-25 18:13:13 +0100

    adding allow-none introspection metadata to pango_parse_enum

--- pango/pango-utils.c
+++ pango/pango-utils.c
@@ -773,10 +773,10 @@ parse_int (const char *word,
 /**
  * pango_parse_enum:
  * @type: enum type to parse, eg. %PANGO_TYPE_ELLIPSIZE_MODE.
- * @str: string to parse.  May be %NULL.
- * @value: integer to store the result in, or %NULL.
+ * @str: (allow-none): string to parse.  May be %NULL.
+ * @value: (allow-none): integer to store the result in, or %NULL.
  * @warn: if %TRUE, issue a g_warning() on bad input.
- * @possible_values: place to store list of possible values on failure, or %NULL.
+ * @possible_values: (allow-none): place to store list of possible values on failure, or %NULL.
  *
  * Parses an enum type and stores the result in @value.
  *
