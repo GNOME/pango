e3afea7d120f2f2957185840d94aab70da3e2063
Alberto Ruiz <aruiz@gnome.org>
2011-04-25 13:10:56 +0100

    adding output parameters introspection metadata to pango_font_get_glypth_extents

--- pango/fonts.c
+++ pango/fonts.c
@@ -1546,9 +1546,9 @@ pango_font_find_shaper (PangoFont     *font,
  * pango_font_get_glyph_extents:
  * @font: a #PangoFont
  * @glyph: the glyph index
- * @ink_rect: (allowe-none): rectangle used to store the extents of the glyph
+ * @ink_rect: (out) (allowe-none): rectangle used to store the extents of the glyph
  *            as drawn or %NULL to indicate that the result is not needed.
- * @logical_rect (allow-none): rectangle used to store the logical extents of
+ * @logical_rect: (out) (allow-none): rectangle used to store the logical extents of
  *            the glyph or %NULL to indicate that the result is not needed.
  *
  * Gets the logical and ink extents of a glyph within a font. The
