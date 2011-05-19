21ab90b29b7fb5f90bf2fd653f4a9b1a0f661a2a
Behdad Esfahbod <behdad@behdad.org>
2011-05-19 16:43:44 -0400

    [HB] Adjust to more vertical API changes

--- modules/basic/basic-fc.c
+++ modules/basic/basic-fc.c
@@ -158,10 +158,10 @@ pango_fc_hb_font_get_glyph_contour_point (hb_font_t *font, void *font_data,
 #endif
 }
 
-static hb_bool_t
+static void
 pango_fc_hb_font_get_glyph_h_advance (hb_font_t *font, void *font_data,
 				      hb_codepoint_t glyph,
-				      hb_position_t *x_advance, hb_position_t *y_advance G_GNUC_UNUSED,
+				      hb_position_t *advance,
 				      void *user_data G_GNUC_UNUSED)
 {
   PangoFcHbContext *context = (PangoFcHbContext *) font_data;
@@ -170,9 +170,7 @@ pango_fc_hb_font_get_glyph_h_advance (hb_font_t *font, void *font_data,
 
   pango_font_get_glyph_extents ((PangoFont *) fc_font, glyph, NULL, &logical);
 
-  *x_advance = logical.width;
-
-  return TRUE;
+  *advance = logical.width;
 }
 
 static hb_bool_t
@@ -195,10 +193,10 @@ pango_fc_hb_font_get_glyph_extents (hb_font_t *font,  void *font_data,
   return TRUE;
 }
 
-static hb_bool_t
+static void
 pango_fc_hb_font_get_h_kerning (hb_font_t *font, void *font_data,
 				hb_codepoint_t left_glyph, hb_codepoint_t right_glyph,
-				hb_position_t *x_kern, hb_position_t *y_kern,
+				hb_position_t *kern,
 				void *user_data G_GNUC_UNUSED)
 {
   PangoFcHbContext *context = (PangoFcHbContext *) font_data;
@@ -206,12 +204,9 @@ pango_fc_hb_font_get_h_kerning (hb_font_t *font, void *font_data,
   FT_Vector kerning;
 
   if (FT_Get_Kerning (ft_face, left_glyph, right_glyph, FT_KERNING_DEFAULT, &kerning))
-    return FALSE;
+    return;
 
-  *x_kern = PANGO_UNITS_26_6 (kerning.x);
-  *y_kern = PANGO_UNITS_26_6 (kerning.y);
-
-  return TRUE;
+  *kern = PANGO_UNITS_26_6 (kerning.x);
 }
 
 static hb_font_funcs_t *
