GNOME/pango

commit 02f80c9acdec53f58fef0e12f41363c3c4ea1efe

pango/pangoatsui-fontmap.c: Use G_DEFINE_TYPE

pango/pangoatsui-fontmap.c
@@ -70,9 +70,6 @@ struct _PangoATSUIFace
 static GType pango_atsui_family_get_type (void);
 static GType pango_atsui_face_get_type (void);
 
-static gpointer pango_atsui_family_parent_class;
-static gpointer pango_atsui_face_parent_class;
-
 static const char *
 get_real_family (const char *family_name)
 {
@@ -222,13 +219,13 @@ pango_atsui_family_finalize (GObject *object)
   G_OBJECT_CLASS (pango_atsui_family_parent_class)->finalize (object);
 }
 
+G_DEFINE_TYPE (PangoATSUIFamilyClass, pango_atsui_family, PANGO_TYPE_FONT_FAMILY);
+
 static void
-pango_atsui_family_class_init (PangoFontFamilyClass *class)
+pango_atsui_family_class_init (PangoATSUIFamilyClass *class)
 {
   GObjectClass *object_class = (GObjectClass *)class;
   int i;
-
-  pango_atsui_family_parent_class = g_type_class_peek_parent (class);
 
   object_class->finalize = pango_atsui_family_finalize;
 
@@ -244,34 +241,6 @@ static void
 pango_atsui_family_init (PangoATSUIFamily *family)
 {
   family->n_faces = -1;
-}
-
-static GType
-pango_atsui_family_get_type (void)
-{
-  static GType object_type = 0;
-
-  if (G_UNLIKELY (!object_type))
-    {
-      const GTypeInfo object_info =
-      {
-	sizeof (PangoFontFamilyClass),
-	(GBaseInitFunc) NULL,
-	(GBaseFinalizeFunc) NULL,
-	(GClassInitFunc) pango_atsui_family_class_init,
-	NULL,           /* class_finalize */
-	NULL,           /* class_data */
-	sizeof (PangoATSUIFamily),
-	0,              /* n_preallocs */
-	(GInstanceInitFunc) pango_atsui_family_init,
-      };
-
-      object_type = g_type_register_static (PANGO_TYPE_FONT_FAMILY,
-					    I_("PangoATSUIFamily"),
-					    &object_info, 0);
-    }
-
-  return object_type;
 }
 
 static PangoFontDescription *
@@ -367,8 +336,6 @@ static void
 pango_atsui_face_class_init (PangoFontFaceClass *class)
 {
   GObjectClass *object_class = (GObjectClass *)class;
-
-  pango_atsui_face_parent_class = g_type_class_peek_parent (class);
 
   object_class->finalize = pango_atsui_face_finalize;
 

commit cbc881d42159e9a72e6ed408695f59df1a34513e

Make ATSUI backend compile again
Commit 02f80c9acdec53f58fef0e12f41363c3c4ea1efe broke the build and was
likely committed untested.

pango/pangoatsui-fontmap.c
@@ -70,6 +70,8 @@ struct _PangoATSUIFace
 static GType pango_atsui_family_get_type (void);
 static GType pango_atsui_face_get_type (void);
 
+static gpointer pango_atsui_face_parent_class;
+
 static const char *
 get_real_family (const char *family_name)
 {
@@ -200,6 +202,8 @@ pango_atsui_family_is_monospace (PangoFontFamily *family)
   return atsuifamily->is_monospace;
 }
 
+G_DEFINE_TYPE (PangoATSUIFamily, pango_atsui_family, PANGO_TYPE_FONT_FAMILY);
+
 static void
 pango_atsui_family_finalize (GObject *object)
 {
@@ -218,8 +222,6 @@ pango_atsui_family_finalize (GObject *object)
 
   G_OBJECT_CLASS (pango_atsui_family_parent_class)->finalize (object);
 }
-
-G_DEFINE_TYPE (PangoATSUIFamilyClass, pango_atsui_family, PANGO_TYPE_FONT_FAMILY);
 
 static void
 pango_atsui_family_class_init (PangoATSUIFamilyClass *class)
@@ -336,6 +338,8 @@ static void
 pango_atsui_face_class_init (PangoFontFaceClass *class)
 {
   GObjectClass *object_class = (GObjectClass *)class;
+
+  pango_atsui_face_parent_class = g_type_class_peek_parent (class);
 
   object_class->finalize = pango_atsui_face_finalize;
 

pango/pangoatsui-private.h
@@ -38,6 +38,8 @@ G_BEGIN_DECLS
 
 
 typedef struct _PangoATSUIFamily       PangoATSUIFamily;
+typedef struct _PangoFontFamilyClass   PangoATSUIFamilyClass;
+
 typedef struct _PangoATSUIFace         PangoATSUIFace;
 
 typedef struct _PangoATSUIFontMap      PangoATSUIFontMap;