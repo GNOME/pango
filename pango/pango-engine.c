GNOME/pango

commit ea702f4c4feac29e6366a04c02953c425bb5f288

pango/pango-engine.c: Use G_DEFINE_TYPE instead custom macro

pango/pango-engine.c
@@ -25,13 +25,30 @@
 #include "pango-engine-private.h"
 #include "pango-impl-utils.h"
 
-PANGO_DEFINE_TYPE_ABSTRACT (PangoEngine, pango_engine,
-			    NULL, NULL,
-			    G_TYPE_OBJECT)
+static void
+pango_engine_init (PangoEngine *self)
+{
+}
 
-PANGO_DEFINE_TYPE_ABSTRACT (PangoEngineLang, pango_engine_lang,
-			    NULL, NULL,
-			    PANGO_TYPE_ENGINE)
+static void
+pango_engine_class_init (PangoEngineClass *klass)
+{
+}
+
+G_DEFINE_ABSTRACT_TYPE (PangoEngine, pango_engine, G_TYPE_OBJECT);
+
+
+static void
+pango_engine_lang_init (PangoEngineLang *self)
+{
+}
+
+static void
+pango_engine_lang_class_init (PangoEngineLangClass *klass)
+{
+}
+
+G_DEFINE_ABSTRACT_TYPE (PangoEngineLang, pango_engine_lang, PANGO_TYPE_ENGINE);
 
 static PangoCoverageLevel
 pango_engine_shape_real_covers (PangoEngineShape *engine G_GNUC_UNUSED,
@@ -49,14 +66,17 @@ pango_engine_shape_real_covers (PangoEngineShape *engine G_GNUC_UNUSED,
 }
 
 static void
+pango_engine_shape_init (PangoEngineShape *klass)
+{
+}
+
+static void
 pango_engine_shape_class_init (PangoEngineShapeClass *class)
 {
   class->covers = pango_engine_shape_real_covers;
 }
 
-PANGO_DEFINE_TYPE_ABSTRACT (PangoEngineShape, pango_engine_shape,
-			    pango_engine_shape_class_init, NULL,
-			    PANGO_TYPE_ENGINE)
+G_DEFINE_ABSTRACT_TYPE (PangoEngineShape, pango_engine_shape, PANGO_TYPE_ENGINE);
 
 void
 _pango_engine_shape_shape (PangoEngineShape *engine,
@@ -155,15 +175,18 @@ fallback_engine_covers (PangoEngineShape *engine G_GNUC_UNUSED,
 }
 
 static void
-fallback_engine_class_init (PangoEngineShapeClass *class)
+pango_fallback_engine_init (PangoFallbackEngine *self)
+{
+}
+
+static void
+pango_fallback_engine_class_init (PangoFallbackEngineClass *class)
 {
   class->covers = fallback_engine_covers;
   class->script_shape = fallback_engine_shape;
 }
 
-static PANGO_DEFINE_TYPE (PangoFallbackEngine, pango_fallback_engine,
-			  fallback_engine_class_init, NULL,
-			  PANGO_TYPE_ENGINE_SHAPE)
+G_DEFINE_ABSTRACT_TYPE (PangoFallbackEngine, pango_fallback_engine, PANGO_TYPE_ENGINE_SHAPE);
 
 PangoEngineShape *
 _pango_get_fallback_shaper (void)

commit 069767e1f71a0a83c7abd5fba07f7c60a9956667

Part of Bug 653985 - Pango 1.29.x make GTK3-demo.exe crashed in Windows
Fixes a regression from ea702f4 which changed PangoFallbackEngine to be
abstract.

pango/pango-engine.c
@@ -180,7 +180,7 @@ fallback_engine_covers (PangoEngineShape *engine G_GNUC_UNUSED,
 
 static GType pango_fallback_engine_get_type (void);
 
-G_DEFINE_ABSTRACT_TYPE (PangoFallbackEngine, pango_fallback_engine, PANGO_TYPE_ENGINE_SHAPE);
+G_DEFINE_TYPE (PangoFallbackEngine, pango_fallback_engine, PANGO_TYPE_ENGINE_SHAPE);
 
 static void
 pango_fallback_engine_init (PangoFallbackEngine *self)