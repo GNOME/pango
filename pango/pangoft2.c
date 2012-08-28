4ea16a17ca32d11bb3fd59132823350a77568b70
Colin Walters <walters@verbum.org>
2012-08-28 08:17:37 -0400

    pangoft2: Fix typo

--- pango/pangoft2.c
+++ pango/pangoft2.c
@@ -545,7 +545,7 @@ _pango_ft2_ft_strerror (FT_Error error)
       static char *default_msg = NULL; /* MT-safe */
 
       if (g_once_init_enter (&default_msg))
-	g_once_init_leve (&default_msg, g_malloc (60));
+	g_once_init_leave (&default_msg, g_malloc (60));
 
       g_sprintf (default_msg, "Unknown FreeType2 error %#x", error);
       return default_msg;
