88e0d1599cb0307fe93cf31b36f654303422b411
Matthias Clasen <mclasen@redhat.com>
2013-01-07 21:05:41 -0500

    Avoid compiler warnings in testiter.c
    Drop an unused variable, and don't call the deprecated
    pango_cairo_font_map_create_context().

--- tests/testiter.c
+++ tests/testiter.c
@@ -165,11 +165,8 @@ iter_cluster_test (PangoLayout *layout)
   PangoLayoutIter *iter;
   int              index;
   gboolean         iter_next_ok;
-  const char      *text;
   PangoLayoutLine *last_line = NULL;
   int              expected_next_x = 0;
-
-  text = pango_layout_get_text (layout);
 
   iter = pango_layout_get_iter (layout);
   iter_next_ok = TRUE;
@@ -221,7 +218,7 @@ main (int argc, char *argv[])
   g_setenv ("PANGO_RC_FILE", "./pangorc", TRUE);
 
   fontmap = pango_cairo_font_map_get_default ();
-  context = pango_cairo_font_map_create_context (PANGO_CAIRO_FONT_MAP (fontmap));
+  context = pango_font_map_create_context (fontmap);
 
   layout = pango_layout_new (context);
   pango_layout_set_width (layout, LAYOUT_WIDTH);
