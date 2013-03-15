e95a0ecd5d1bbcc0c96fc52160edc18292f66f04
Behdad Esfahbod <behdad@behdad.org>
2013-03-15 05:56:14 -0400

    Use g_snprintf()

--- tests/test-pangocairo-threads.c
+++ tests/test-pangocairo-threads.c
@@ -89,7 +89,7 @@ main (int argc, char **argv)
       char buf[10];
       cairo_surface_t *surface = create_surface ();
       g_ptr_array_add (surfaces, surface);
-      snprintf (buf, sizeof (buf), "%d", i);
+      g_snprintf (buf, sizeof (buf), "%d", i);
       g_ptr_array_add (threads,
 		       g_thread_new (buf,
 				     thread_func,
