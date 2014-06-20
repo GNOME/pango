4901fb74b3c1137d13a6b7e4e8784577c1816e47
Emmanuele Bassi <ebassi@gnome.org>
2014-06-20 11:39:15 +0100

    ot-info: Update after API break in HarfBuzz

--- pango/pango-ot-info.c
+++ pango/pango-ot-info.c
@@ -189,10 +189,11 @@ pango_ot_info_find_language (PangoOTInfo      *info,
 					   &l_index);
   if (language_index) *language_index = l_index;
 
-  hb_ot_layout_language_get_required_feature_index (info->hb_face, tt,
-						    script_index,
-						    l_index,
-						    required_feature_index);
+  hb_ot_layout_language_get_required_feature (info->hb_face, tt,
+                                              script_index,
+                                              l_index,
+                                              required_feature_index,
+                                              NULL);
 
   return ret;
 }
