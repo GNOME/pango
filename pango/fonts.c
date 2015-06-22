747fbdb28b7f10265ff1bbbbc456163a3d2506b1
Matthias Clasen <mclasen@redhat.com>
2015-06-22 08:13:33 -0400

    Minor documentation update

--- pango/fonts.c
+++ pango/fonts.c
@@ -1080,7 +1080,7 @@ parse_size (const char *word,
  * Creates a new font description from a string representation in the
  * form "[FAMILY-LIST] [STYLE-OPTIONS] [SIZE]", where FAMILY-LIST is a
  * comma separated list of families optionally terminated by a comma,
- * STYLE_OPTIONS is a whitespace separated list of words where each WORD
+ * STYLE_OPTIONS is a whitespace separated list of words where each word
  * describes one of style, variant, weight, stretch, or gravity, and SIZE
  * is a decimal number (size in points) or optionally followed by the
  * unit modifier "px" for absolute size. Any one of the options may
