cce034fea4187ce02577dbc6d92c8b1042aca815
Chun-wei Fan <fanchunwei@src.gnome.org>
2016-08-22 01:08:47 +0800

    pango/pango-version-macros.h: Update copyright notice
    Update the copyright notice of this file, to reflect things more
    accurately and to give more proper acknowledgement.
    
    Please see: https://bugzilla.gnome.org/show_bug.cgi?id=767587#c16

--- pango/pango-version-macros.h
+++ pango/pango-version-macros.h
@@ -1,15 +1,17 @@
 /* Pango - Internationalized text layout and rendering library
- * Copyright (C) 2016  Chun-wei Fan
+ * Copyright (C) 1999 Red Hat Software
+ * Copyright (C) 2012 Ryan Lortie, Matthias Clasen and Emmanuele Bassi
+ * Copyright (C) 2016 Chun-wei Fan
  *
  * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
+ * modify it under the terms of the GNU Library General Public
  * License as published by the Free Software Foundation; either
  * version 2 of the License, or (at your option) any later version.
  *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
+ * Library General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, see <http://www.gnu.org/licenses/>.
