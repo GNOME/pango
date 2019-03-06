759278c2f37fde6c1318d62660515ea77e19b317
Akira TAGOH <akira@tagoh.org>
2019-03-06 11:18:49 +0000

    Deal with Common scripts as und-zyyy language tag.
    When fonts being rendered for Common has different looks to Latin,
    Pango changes a font and makes different looks for a cluster of words
    just after tying a character. e.g. 012345 vs 012345a.
    
    This is a long standing UX problem in Pango and they should be rendered
    with the same font all the time.
    
    This change tries to fix this issue and leave customization to fontconfig
    layer.
    
    Fixes https://gitlab.gnome.org/GNOME/pango/issues/353

--- pango/pango-script.c
+++ pango/pango-script.c
@@ -106,7 +106,7 @@ _pango_script_iter_init (PangoScriptIter *iter,
 
   iter->script_start = text;
   iter->script_end = text;
-  iter->script_code = PANGO_SCRIPT_COMMON;
+  iter->script_code = PANGO_SCRIPT_UNKNOWN;
 
   iter->paren_sp = -1;
 
@@ -229,7 +229,8 @@ get_pair_index (gunichar ch)
 
 /* duplicated in pango-language.c */
 #define REAL_SCRIPT(script) \
-  ((script) > PANGO_SCRIPT_INHERITED && (script) != PANGO_SCRIPT_UNKNOWN)
+  (((script) > PANGO_SCRIPT_INHERITED && (script) != PANGO_SCRIPT_UNKNOWN) || \
+   (script) == PANGO_SCRIPT_UNKNOWN)
 
 #define SAME_SCRIPT(script1, script2) \
   (!REAL_SCRIPT (script1) || !REAL_SCRIPT (script2) || (script1) == (script2))
@@ -257,7 +258,7 @@ pango_script_iter_next (PangoScriptIter *iter)
     return FALSE;
 
   start_sp = iter->paren_sp;
-  iter->script_code = PANGO_SCRIPT_COMMON;
+  iter->script_code = PANGO_SCRIPT_UNKNOWN;
   iter->script_start = iter->script_end;
 
   for (; iter->script_end < iter->text_end; iter->script_end = g_utf8_next_char (iter->script_end))
