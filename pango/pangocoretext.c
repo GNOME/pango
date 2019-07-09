22459facc9e0b61965243cff40c454121fb78386
Matthias Clasen <mclasen@redhat.com>
2019-07-09 15:05:33 -0400

    coretext: Stop providing a shape engine
    It is no longer used.

--- pango/pangocoretext.c
+++ pango/pangocoretext.c
@@ -160,46 +160,6 @@ pango_core_text_font_get_coverage (PangoFont     *font,
   return pango_coverage_ref (priv->coverage);
 }
 
-/* Wrap shaper in PangoEngineShape to pass it through old API,
- * from times when there were modules and engines. */
-typedef PangoEngineShape      PangoCoreTextShapeEngine;
-typedef PangoEngineShapeClass PangoCoreTextShapeEngineClass;
-static GType pango_core_text_shape_engine_get_type (void) G_GNUC_CONST;
-G_DEFINE_TYPE (PangoCoreTextShapeEngine, pango_core_text_shape_engine, PANGO_TYPE_ENGINE_SHAPE);
-static void
-_pango_core_text_shape_engine_shape (PangoEngineShape    *engine G_GNUC_UNUSED,
-				 PangoFont           *font,
-				 const char          *item_text,
-				 unsigned int         item_length,
-				 const PangoAnalysis *analysis,
-				 PangoGlyphString    *glyphs,
-				 const char          *paragraph_text,
-				 unsigned int         paragraph_length)
-{
-  _pango_core_text_shape (font, item_text, item_length, analysis, glyphs,
-		      paragraph_text, paragraph_length);
-}
-static void
-pango_core_text_shape_engine_class_init (PangoEngineShapeClass *class)
-{
-  class->script_shape = _pango_core_text_shape_engine_shape;
-}
-static void
-pango_core_text_shape_engine_init (PangoEngineShape *object)
-{
-}
-
-static PangoEngineShape *
-pango_core_text_font_find_shaper (PangoFont     *font,
-                                  PangoLanguage *language G_GNUC_UNUSED,
-                                  guint32        ch)
-{
-  static PangoEngineShape *shaper;
-  if (g_once_init_enter (&shaper))
-    g_once_init_leave (&shaper, g_object_new (pango_core_text_shape_engine_get_type(), NULL));
-  return shaper;
-}
-
 static PangoFontMap *
 pango_core_text_font_get_font_map (PangoFont *font)
 {
@@ -225,7 +185,6 @@ pango_core_text_font_class_init (PangoCoreTextFontClass *class)
   font_class->describe = pango_core_text_font_describe;
   /* font_class->describe_absolute is left virtual for PangoCairoCoreTextFont. */
   font_class->get_coverage = pango_core_text_font_get_coverage;
-  font_class->find_shaper = pango_core_text_font_find_shaper;
   font_class->get_font_map = pango_core_text_font_get_font_map;
 }
 
