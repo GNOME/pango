b9fa9ccc28d0025637ce53312a3f40f354cbd56d
Matthias Clasen <mclasen@redhat.com>
2019-07-16 16:32:04 -0400

    Try one more time

--- .gitlab-ci/list-msys2.sh
+++ .gitlab-ci/list-msys2.sh
@@ -2,5 +2,5 @@
 
 set -e
 
-PATH=_build/pango:$PATH _build/tests/test-font -p /pango/font/metrics --verbose
+PATH=_build/pango:$PATH _build/tests/test-font -p /pango/font/metrics --verbose >&_build/fontmetrics.txt
 PATH=_build/pango:$PATH _build/utils/pango-list --verbose > _build/fontlist.txt
