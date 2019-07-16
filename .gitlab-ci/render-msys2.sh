461a5842ddbb351d9511605e5a59c268fbc32c38
Matthias Clasen <mclasen@redhat.com>
2019-07-16 15:04:03 -0400

    ci: Render without line spacing
    To confirm whether this is the problem on win32.

--- .gitlab-ci/render-msys2.sh
+++ .gitlab-ci/render-msys2.sh
@@ -2,4 +2,4 @@
 
 set -e
 
-PATH=_build/pango:$PATH _build/utils/pango-view --no-display --output _build/hello.png utils/HELLO.txt
+PATH=_build/pango:$PATH _build/utils/pango-view --line-spacing 0 --no-display --output _build/hello.png utils/HELLO.txt
