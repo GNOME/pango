b43a9557db67ae54b9d16c9cd6f0d2476cd9f093
Matthias Clasen <mclasen@redhat.com>
2019-08-16 08:40:53 -0400

    Add 1.45 version macros

--- pango/pango-version-macros.h
+++ pango/pango-version-macros.h
@@ -252,6 +252,16 @@
  */
 #define PANGO_VERSION_1_44       (G_ENCODE_VERSION (1, 44))
 
+/**
+ * PANGO_VERSION_1_45:
+ *
+ * A macro that evaluates to the 1.45 version of Pango, in a format
+ * that can be used by the C pre-processor.
+ *
+ * Since: 1.45
+ */
+#define PANGO_VERSION_1_45       (G_ENCODE_VERSION (1, 45))
+
 /* evaluates to the current stable version; for development cycles,
  * this means the next stable target
  */
@@ -655,6 +665,20 @@
 # define PANGO_AVAILABLE_IN_1_44                PANGO_UNAVAILABLE(1, 44)
 #else
 # define PANGO_AVAILABLE_IN_1_44                _PANGO_EXTERN
+#endif
+
+#if PANGO_VERSION_MIN_REQUIRED >= PANGO_VERSION_1_45
+# define PANGO_DEPRECATED_IN_1_45               PANGO_DEPRECATED
+# define PANGO_DEPRECATED_IN_1_45_FOR(f)        PANGO_DEPRECATED_FOR(f)
+#else
+# define PANGO_DEPRECATED_IN_1_45               _PANGO_EXTERN
+# define PANGO_DEPRECATED_IN_1_45_FOR(f)        _PANGO_EXTERN
+#endif
+
+#if PANGO_VERSION_MAX_ALLOWED < PANGO_VERSION_1_45
+# define PANGO_AVAILABLE_IN_1_45                PANGO_UNAVAILABLE(1, 45)
+#else
+# define PANGO_AVAILABLE_IN_1_45                _PANGO_EXTERN
 #endif
 
 #endif /* __PANGO_VERSION_H__ */
