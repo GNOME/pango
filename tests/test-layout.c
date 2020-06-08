fb8664764b6d2432a8f91f309439b00a60f07106
Emmanuele Bassi <ebassi@gnome.org>
2020-06-08 17:08:08 +0100

    Skip layout tests if the en_US locale is not available
    There's no point in running a tests if we know the locale isn't there.
    The reason gets logged, so the developer can decide to install it.

--- tests/test-layout.c
+++ tests/test-layout.c
@@ -304,6 +304,16 @@ test_layout (gconstpointer d)
   GString *dump;
   gchar *diff;
 
+  const char *old_locale = setlocale (LC_ALL, NULL);
+  setlocale (LC_ALL, "en_US.UTF-8");
+  if (strstr (setlocale (LC_ALL, NULL), "en_US") == NULL)
+    {
+      char *msg = g_strdup_printf ("Locale en_US.UTF-8 not available, skipping layout %s", filename);
+      g_test_skip (msg);
+      g_free (msg);
+      return;
+    }
+
   expected_file = get_expected_filename (filename);
 
   dump = g_string_sized_new (0);
@@ -323,6 +333,8 @@ test_layout (gconstpointer d)
 
   g_string_free (dump, TRUE);
   g_free (expected_file);
+
+  setlocale (LC_ALL, old_locale);
 }
 
 int
@@ -332,9 +344,6 @@ main (int argc, char *argv[])
   GError *error = NULL;
   const gchar *name;
   gchar *path;
-
-  g_setenv ("LC_ALL", "en_US.UTF-8", TRUE);
-  setlocale (LC_ALL, "");
 
   g_test_init (&argc, &argv, NULL);
 
