GNOME/pango

commit f6032b754fd76ebbf0ae6ec4f3c8b2616add0e91

tests: Update the locale use
Create the PangoContext for the test after resetting the locale, and use
the en_US.utf8 name, as it seems to be the name used by Fedora.

tests/test-layout.c
@@ -305,7 +305,7 @@ test_layout (gconstpointer d)
   gchar *diff;
 
   const char *old_locale = setlocale (LC_ALL, NULL);
-  setlocale (LC_ALL, "en_US.UTF-8");
+  setlocale (LC_ALL, "en_US.utf8");
   if (strstr (setlocale (LC_ALL, NULL), "en_US") == NULL)
     {
       char *msg = g_strdup_printf ("Locale en_US.UTF-8 not available, skipping layout %s", filename);
@@ -313,6 +313,9 @@ test_layout (gconstpointer d)
       g_free (msg);
       return;
     }
+
+  if (context == NULL)
+    context = pango_font_map_create_context (pango_cairo_font_map_get_default ());
 
   expected_file = get_expected_filename (filename);
 
@@ -354,8 +357,6 @@ main (int argc, char *argv[])
   gchar *path;
 
   g_test_init (&argc, &argv, NULL);
-
-  context = pango_font_map_create_context (pango_cairo_font_map_get_default ());
 
   /* allow to easily generate expected output for new test cases */
   if (argc > 1)

tests/test-break.c
@@ -242,7 +242,7 @@ test_break (gconstpointer d)
   gchar *diff;
 
   const char *old_locale = setlocale (LC_ALL, NULL);
-  setlocale (LC_ALL, "en_US.UTF-8");
+  setlocale (LC_ALL, "en_US.utf8");
   if (strstr (setlocale (LC_ALL, NULL), "en_US") == NULL)
     {
       char *msg = g_strdup_printf ("Locale en_US.UTF-8 not available, skipping break %s", filename);
@@ -250,6 +250,9 @@ test_break (gconstpointer d)
       g_free (msg);
       return;
     }
+
+  if (context == NULL)
+    context = pango_font_map_create_context (pango_cairo_font_map_get_default ());
 
   expected_file = get_expected_filename (filename);
 
@@ -292,8 +295,6 @@ main (int argc, char *argv[])
   gchar *path;
 
   g_test_init (&argc, &argv, NULL);
-
-  context = pango_font_map_create_context (pango_cairo_font_map_get_default ());
 
   /* allow to easily generate expected output for new test cases */
   if (argc > 1)

tests/test-itemize.c
@@ -238,7 +238,7 @@ test_itemize (gconstpointer d)
   gchar *diff;
 
   const char *old_locale = setlocale (LC_ALL, NULL);
-  setlocale (LC_ALL, "en_US.UTF-8");
+  setlocale (LC_ALL, "en_US.utf8");
   if (strstr (setlocale (LC_ALL, NULL), "en_US") == NULL)
     {
       char *msg = g_strdup_printf ("Locale en_US.UTF-8 not available, skipping itemization %s", filename);
@@ -246,6 +246,8 @@ test_itemize (gconstpointer d)
       g_free (msg);
       return;
     }
+
+  context = pango_font_map_create_context (pango_cairo_font_map_get_default ());
 
   expected_file = get_expected_filename (filename);
 
@@ -288,8 +290,6 @@ main (int argc, char *argv[])
   gchar *path;
 
   g_test_init (&argc, &argv, NULL);
-
-  context = pango_font_map_create_context (pango_cairo_font_map_get_default ());
 
   /* allow to easily generate expected output for new test cases */
   if (argc > 1)

commit 553841ac75aa69f562391b98b983ec4881a89855

Make test-layout work for generating data again
This was broken in f6032b754fd76ebbf0ae6.

tests/test-layout.c
@@ -238,6 +238,9 @@ test_file (const gchar *filename, GString *string)
   PangoWrapMode wrap = PANGO_WRAP_WORD;
   PangoFontDescription *desc;
 
+  if (context == NULL)
+    context = pango_font_map_create_context (pango_cairo_font_map_get_default ());
+
   g_file_get_contents (filename, &contents, &length, &error);
   g_assert_no_error (error);
 
@@ -359,12 +362,12 @@ main (int argc, char *argv[])
   const gchar *name;
   gchar *path;
 
-  g_test_init (&argc, &argv, NULL);
-
   /* allow to easily generate expected output for new test cases */
-  if (argc > 1)
+  if (argc > 1 && argv[1][0] != '-')
     {
       GString *string;
+
+      setlocale (LC_ALL, "en_US.utf8");
 
       string = g_string_sized_new (0);
       test_file (argv[1], string);
@@ -372,6 +375,8 @@ main (int argc, char *argv[])
 
       return 0;
     }
+
+  g_test_init (&argc, &argv, NULL);
 
   path = g_test_build_filename (G_TEST_DIST, "layouts", NULL);
   dir = g_dir_open (path, 0, &error);