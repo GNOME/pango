221a35e6546f306927749a212f6c8a95ca7d8e46
Simon McVittie <smcv@debian.org>
2020-09-25 16:12:01 +0100

    Revert "testsuite: Don't use g_test_skip"
    When versions of Meson with bug
    https://github.com/mesonbuild/meson/issues/7515 are no longer widespread,
    the Pango test suite should diagnose which tests are skipped again.
    Versions 0.55.0 to 0.55.2 inclusive have the bug; version 0.55.3 is OK.
    
    This reverts commit 440f9c2609b7f5f9d8f65619b029a5eb660de88b.
    
    Signed-off-by: Simon McVittie <smcv@debian.org>

--- tests/testboundaries_ucd.c
+++ tests/testboundaries_ucd.c
@@ -232,9 +232,7 @@ do_test (const gchar *filename,
   channel = g_io_channel_new_file (filename, "r", &error);
   if (g_error_matches (error, G_FILE_ERROR, G_FILE_ERROR_NOENT))
     {
-#if 0
       g_test_skip ("Test file not found");
-#endif
       return;
     }
 
