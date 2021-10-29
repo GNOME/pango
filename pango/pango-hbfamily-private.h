3ea840e01ac9d03347887c6043c0628f4770b9f4
Matthias Clasen <mclasen@redhat.com>
2021-10-29 19:05:34 -0400

    Add PangoHbFamily
    This is a straightforward implementation of
    PangoFontFamily for use with PangoHbFace.

---
+++ pango/pango-hbfamily-private.h
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2021 Red Hat, Inc.
+ *
+ * SPDX-License-Identifier: LGPL-2.1-or-later
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include "pango-font-family-private.h"
+#include "pango-hbface.h"
+
+
+#define PANGO_TYPE_HB_FAMILY (pango_hb_family_get_type ())
+
+G_DECLARE_FINAL_TYPE (PangoHbFamily, pango_hb_family, PANGO, HB_FAMILY, PangoFontFamily)
+
+struct _PangoHbFamily
+{
+  PangoFontFamily parent_instance;
+
+  PangoFontMap *map;
+  char *name;
+  GPtrArray *faces;
+};
+
+PangoHbFamily *         pango_hb_family_new             (const char             *name);
+
+void                    pango_hb_family_set_font_map    (PangoHbFamily          *self,
+                                                         PangoFontMap           *map);
+
+void                    pango_hb_family_add_face        (PangoHbFamily          *self,
+                                                         PangoHbFace            *face);
+
+void                    pango_hb_family_remove_face     (PangoHbFamily          *self,
+                                                         PangoHbFace            *face);
+
+PangoHbFace *           pango_hb_family_find_face       (PangoHbFamily          *self,
+                                                         PangoFontDescription   *description,
+                                                         PangoLanguage          *language,
+                                                         gunichar                wc);
