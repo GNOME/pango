09996f6395f350f2cf8a4da9c09fccf5d783d6c3
Matthias Clasen <mclasen@redhat.com>
2021-11-16 23:54:58 -0500

    Use serialization api in testattributes

--- tests/test-common.h
+++ tests/test-common.h
@@ -19,12 +19,6 @@ void print_attributes (GSList        *attrs,
 void print_attr_list (PangoAttrList  *attrs,
                       GString        *string);
 
-PangoAttribute *
-attribute_from_string (const char *string);
-
-PangoAttrList *
-attributes_from_string (const char *string);
-
 const char *get_script_name (GUnicodeScript s);
 
 
