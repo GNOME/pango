48c25f19005a44a32af650b3cf4b0d4695eb36cb
Matthias Clasen <mclasen@redhat.com>
2022-01-01 21:15:09 -0500

    Add PangoHbFontmap
    This is a fontmap implementation for use with PangoHbFont,
    which lets you add individual faces. It handles caching
    and lookups and is designed to let subclasses simply provide
    a populate vfunc.
    
    Note that the load_fontset implementation here is similar in
    style to the one we have in PangoFontMap: for each family
    in the font description, pick the best matching face.

--- pango/pango.h
+++ pango/pango.h
@@ -44,6 +44,7 @@
 #include <pango/pango-gravity.h>
 #include <pango/pango-hbface.h>
 #include <pango/pango-hbfont.h>
+#include <pango/pango-hbfontmap.h>
 #include <pango/pango-item.h>
 #include <pango/pango-language.h>
 #include <pango/pango-layout.h>
