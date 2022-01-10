85f599bb84b614fff98465ccd56bb1af16083fd2
Matthias Clasen <mclasen@redhat.com>
2022-01-10 13:03:15 -0500

    Drop deprecated PangoScript api

--- pango/pango-script.h
+++ pango/pango-script.h
@@ -10,7 +10,7 @@
  *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	 See the GNU
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Library General Public License for more details.
  *
  * You should have received a copy of the GNU Library General Public
@@ -23,6 +23,8 @@
 #define __PANGO_SCRIPT_H__
 
 #include <glib-object.h>
+
+#include <pango/pango-version-macros.h>
 
 G_BEGIN_DECLS
 
@@ -304,17 +306,12 @@ typedef enum {                         /* ISO 15924 code */
       PANGO_SCRIPT_SIGNWRITING             /* Sgnw */
 } PangoScript;
 
-#include <pango/pango-version-macros.h>
-
-PANGO_DEPRECATED_IN_1_44_FOR(g_unichar_get_script)
-PangoScript pango_script_for_unichar         (gunichar             ch) G_GNUC_CONST;
-
 PANGO_AVAILABLE_IN_1_44
 GType            pango_script_iter_get_type  (void) G_GNUC_CONST;
 
 PANGO_AVAILABLE_IN_1_4
 PangoScriptIter *pango_script_iter_new       (const char          *text,
-					      int                  length);
+                                              int                  length);
 PANGO_AVAILABLE_IN_1_4
 void             pango_script_iter_get_range (PangoScriptIter     *iter,
                                               const char         **start,
