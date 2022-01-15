5f1d90cab32276cb867846c17931a0fc18fa23f4
Matthias Clasen <mclasen@redhat.com>
2022-01-14 22:09:57 -0500

    Add PangoLines
    This is an array of positioned PangoLine
    objects.

--- pango/pango.h
+++ pango/pango.h
@@ -47,6 +47,7 @@
 #include <pango/pango-layout-run.h>
 #include <pango/pango-line.h>
 #include <pango/pango-line-breaker.h>
+#include <pango/pango-lines.h>
 #include <pango/pango-matrix.h>
 #include <pango/pango-markup.h>
 #include <pango/pango-renderer.h>
