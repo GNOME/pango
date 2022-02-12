dcfcde31bffc46892d99a144c3b06a4e9ee08654
Matthias Clasen <mclasen@redhat.com>
2022-02-12 10:06:41 -0500

    Move PangoFontDescription to its own files

--- pango/pango.h
+++ pango/pango.h
@@ -31,6 +31,7 @@
 #include <pango/pango-enum-types.h>
 #include <pango/pango-features.h>
 #include <pango/pango-font.h>
+#include <pango/pango-font-description.h>
 #include <pango/pango-fontmap.h>
 #include <pango/pango-fontset.h>
 #include <pango/pango-fontset-simple.h>
