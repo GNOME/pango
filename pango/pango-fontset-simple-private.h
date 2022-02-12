5914c5e88c371dd16206a666284ca9afdc75211c
Matthias Clasen <mclasen@redhat.com>
2022-02-12 14:52:15 -0500

    Make PangoFontsetClass private

--- pango/pango-fontset-simple-private.h
+++ pango/pango-fontset-simple-private.h
@@ -22,6 +22,7 @@
 #ifndef __PANGO_FONTSET_SIMPLE_PRIVATE_H__
 #define __PANGO_FONTSET_SIMPLE_PRIVATE_H__
 
+#include <pango/pango-fontset-private.h>
 #include <pango/pango-fontset-simple.h>
 
 G_BEGIN_DECLS
