dd3fda12685061c64bc74933f6c1da3d28244459
Matthias Clasen <mclasen@redhat.com>
2022-06-05 08:17:24 -0400

    Add a check for mixed linkage
    Having both pango 1.x and pango 2 linked into
    the same process will cause trouble, because the
    type and function names conflict.
    
    Error out if we detect this situation.

--- pango/pango-context.c
+++ pango/pango-context.c
@@ -81,10 +81,31 @@ pango_context_init (PangoContext *context)
   pango_font_description_set_size (context->font_desc, 12 * PANGO_SCALE);
 }
 
+static gboolean
+pango_has_mixed_deps (void)
+{
+  GModule *module;
+  gpointer func;
+  gboolean result = FALSE;
+
+  module = g_module_open (NULL, 0);
+
+  if (g_module_symbol (module, "pango_ot_info_find_script", &func))
+    result = TRUE;
+
+  g_module_close (module);
+
+  return result;
+}
+
 static void
 pango_context_class_init (PangoContextClass *klass)
 {
   GObjectClass *object_class = G_OBJECT_CLASS (klass);
+
+  /* Put the check for mixed linkage here, for lack of a better place */
+  if (pango_has_mixed_deps ())
+    g_error ("Pango 1.x symbols detected.\nUsing Pango 1.x and 2 in the same process is not supported."); 
 
   object_class->finalize = pango_context_finalize;
 }
