fa6ea68aa365662027cd66a91235ec82f8a235fb
Matthias Clasen <mclasen@redhat.com>
2022-06-08 21:21:58 -0400

    Thin out pangocairo code
    We don't use the PangoCairoFontmap or PangoCairoFont
    interfaces anymore. A bunch of code can go.

--- pango/pangocairo-private.h
+++ pango/pangocairo-private.h
@@ -22,13 +22,9 @@
 #include <pango/pangocairo.h>
 #include <pango/pango-renderer.h>
 
-#define PANGO_CAIRO_FONT_GET_IFACE(obj) (G_TYPE_INSTANCE_GET_INTERFACE ((obj), PANGO_TYPE_CAIRO_FONT, PangoCairoFontIface))
-
-typedef struct _PangoCairoFontIface                  PangoCairoFontIface;
 typedef struct _PangoCairoFontPrivate                PangoCairoFontPrivate;
 typedef struct _HexBoxInfo                           PangoCairoFontHexBoxInfo;
 typedef struct _PangoCairoFontPrivateScaledFontData  PangoCairoFontPrivateScaledFontData;
-typedef struct _PangoCairoFontGlyphExtentsCacheEntry PangoCairoFontGlyphExtentsCacheEntry;
 
 struct _PangoCairoFontPrivateScaledFontData
 {
@@ -39,7 +35,7 @@ struct _PangoCairoFontPrivateScaledFontData
 
 struct _PangoCairoFontPrivate
 {
-  PangoCairoFont *cfont;
+  PangoFont *cfont;
 
   PangoCairoFontPrivateScaledFontData *data;
 
@@ -50,43 +46,11 @@ struct _PangoCairoFontPrivate
   PangoGravity gravity;
 
   PangoRectangle font_extents;
-  PangoCairoFontGlyphExtentsCacheEntry *glyph_extents_cache;
-
-  GSList *metrics_by_lang;
-};
-
-struct _PangoCairoFontIface
-{
-  GTypeInterface g_iface;
-
-  cairo_font_face_t *(*create_font_face) (PangoCairoFont *cfont);
-  PangoFontMetrics *(*create_base_metrics_for_context) (PangoCairoFont *cfont,
-                                                        PangoContext   *context);
-
-  gssize cf_priv_offset;
 };
 
 gboolean _pango_cairo_font_install (PangoFont *font,
                                     cairo_t   *cr);
-PangoFontMetrics * _pango_cairo_font_get_metrics (PangoFont     *font,
-                                                  PangoLanguage *language);
 PangoCairoFontHexBoxInfo *_pango_cairo_font_get_hex_box_info (PangoFont *font);
-PangoCairoFontPrivate    *_pango_font_get_cairo_font_private (PangoFont *font);
-cairo_scaled_font_t      *_pango_font_get_scaled_font        (PangoFont *font);
-
-void _pango_cairo_font_private_initialize (PangoCairoFontPrivate      *cf_priv,
-                                           PangoCairoFont             *font,
-                                           PangoGravity                gravity,
-                                           const cairo_font_options_t *font_options,
-                                           const PangoMatrix          *pango_ctm,
-                                           const cairo_matrix_t       *font_matrix);
-void _pango_cairo_font_private_finalize (PangoCairoFontPrivate *cf_priv);
-cairo_scaled_font_t *_pango_cairo_font_private_get_scaled_font (PangoCairoFontPrivate *cf_priv);
-gboolean _pango_cairo_font_private_is_metrics_hinted (PangoCairoFontPrivate *cf_priv);
-void _pango_cairo_font_private_get_glyph_extents (PangoCairoFontPrivate *cf_priv,
-                                                  PangoGlyph             glyph,
-                                                  PangoRectangle        *ink_rect,
-                                                  PangoRectangle        *logical_rect);
 
 #define PANGO_TYPE_CAIRO_RENDERER            (pango_cairo_renderer_get_type())
 #define PANGO_CAIRO_RENDERER(object)         (G_TYPE_CHECK_INSTANCE_CAST ((object), PANGO_TYPE_CAIRO_RENDERER, PangoCairoRenderer))
