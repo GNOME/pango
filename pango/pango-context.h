1bd3fa9394c87b9e67d12a7bf91605a572800f9b
Matthias Clasen <mclasen@redhat.com>
2022-06-11 16:24:03 -0400

    context: Use G_DECLARE_FINAL_TYPE

--- pango/pango-context.h
+++ pango/pango-context.h
@@ -27,18 +27,10 @@
 
 G_BEGIN_DECLS
 
-typedef struct _PangoContextClass PangoContextClass;
-
 #define PANGO_TYPE_CONTEXT              (pango_context_get_type ())
-#define PANGO_CONTEXT(object)           (G_TYPE_CHECK_INSTANCE_CAST ((object), PANGO_TYPE_CONTEXT, PangoContext))
-#define PANGO_CONTEXT_CLASS(klass)      (G_TYPE_CHECK_CLASS_CAST ((klass), PANGO_TYPE_CONTEXT, PangoContextClass))
-#define PANGO_IS_CONTEXT(object)        (G_TYPE_CHECK_INSTANCE_TYPE ((object), PANGO_TYPE_CONTEXT))
-#define PANGO_IS_CONTEXT_CLASS(klass)   (G_TYPE_CHECK_CLASS_TYPE ((klass), PANGO_TYPE_CONTEXT))
-#define PANGO_CONTEXT_GET_CLASS(obj)    (G_TYPE_INSTANCE_GET_CLASS ((obj), PANGO_TYPE_CONTEXT, PangoContextClass))
-
 
 PANGO_AVAILABLE_IN_ALL
-GType                   pango_context_get_type                  (void) G_GNUC_CONST;
+G_DECLARE_FINAL_TYPE (PangoContext, pango_context, PANGO, CONTEXT, GObject);
 
 PANGO_AVAILABLE_IN_ALL
 PangoContext *          pango_context_new                       (void);
