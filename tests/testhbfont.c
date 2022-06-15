d6e561a3632d64d97d3ac03616034248e13890a8
Chun-wei Fan <fanchunwei@src.gnome.org>
2022-06-15 10:27:53 +0800

    testhbfont.c: Fix building on non-FontConfig
    Use the generic pango_font_map_new_default() that accomplishes the same
    thing.  Don't include pangofc-fontmap.h either.

--- tests/testhbfont.c
+++ tests/testhbfont.c
@@ -23,7 +23,6 @@
 #include <glib.h>
 #include <gio/gio.h>
 #include <pango/pangocairo.h>
-#include <pango/pangofc-fontmap.h>
 
 #include <hb-ot.h>
 
@@ -514,7 +513,7 @@ test_fontmap_language (void)
   PangoFontset *fonts;
   gboolean found;
 
-  map = PANGO_FONT_MAP (pango_fc_font_map_new ());
+  map = PANGO_FONT_MAP (pango_font_map_new_default ());
   context = pango_font_map_create_context (map);
   desc = pango_font_description_from_string ("serif 11");
 
