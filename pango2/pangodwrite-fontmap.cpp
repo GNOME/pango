c5bd2b4ceffb6550c5a16edc8ead122b7c46aada
Chun-wei Fan <fanchunwei@src.gnome.org>
2022-07-01 12:31:27 +0800

    Windows: Split out code for aliases (and fallbacks)
    We want to make this code shared between the DirectWrite code and the legacy
    GDI code, since we want to add aliases (and eventually, fallbacks) for all
    cases, at least on Windows (well, font family names do not differ between
    DirectWrite and GDI on a given Windows system :))

--- pango2/pangodwrite-fontmap.cpp
+++ pango2/pangodwrite-fontmap.cpp
@@ -29,13 +29,12 @@
 #include <hb-directwrite.h>
 
 #include "pangodwrite-fontmap.h"
-#include "pango-hbfamily-private.h"
 #include "pango-fontmap-private.h"
 #include "pango-hbface-private.h"
 #include "pango-hbfont-private.h"
 #include "pango-context.h"
-#include "pango-impl-utils.h"
 #include "pango-trace-private.h"
+#include "pangowin32-utils-private.h"
 
 
 /**
@@ -333,37 +332,7 @@ pango2_direct_write_font_map_populate (Pango2FontMap *map)
   collection->Release ();
   collection = NULL;
 
-  /* Add generic aliases */
-  struct {
-    const char *alias_name;
-    const char *family_name;
-  } aliases[] = {
-    { "Monospace",  "Consolas" },
-    { "Sans-serif", "Arial" },
-    { "Sans", "Arial" },
-    { "Serif",      "Times New Roman" },
-    { "System-ui",  "Segoe UI" },
-    { "Emoji",      "Segoe UI Emoji" }
-  };
-
-#if 0
-  if (IsWindows11OrLater ())
-    aliases[0].family_name = "Cascadia Mono";
-#endif
-
-  for (gsize i = 0; i < G_N_ELEMENTS (aliases); i++)
-    {
-      Pango2FontFamily *family = pango2_font_map_get_family (map, aliases[i].family_name);
-
-      if (family)
-        {
-          Pango2GenericFamily *alias_family;
-
-          alias_family = pango2_generic_family_new (aliases[i].alias_name);
-          pango2_generic_family_add_family (alias_family, family);
-          pango2_font_map_add_family (map, PANGO2_FONT_FAMILY (alias_family));
-        }
-    }
+  pango2_win32_font_map_add_aliases_and_fallbacks (map);
 }
 
 /* }}} */
