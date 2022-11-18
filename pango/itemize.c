GNOME/pango

commit a03bf5bc6b07ba6e2442c02d6777978c5cecbd9a

item: Add a char offset
Add a char_offset field to PangoItem, compute it as part of
itemization and update it when splitting items. Keeping this
number around cuts down on the amount of list and utf8 walking
we need to do later.

We have to do some extra shenanigans to preserve abi in the
face of pango's open-coded structs, so we introduce a
PangoItemPrivate type that is used internally. On 64bit,
PangoItem has a 4 byte whole, so we can keep the size of
PangoItemPrivate the same. No such luck on 32bit.

pango/itemize.c
@@ -32,6 +32,7 @@
 #include "pango-script-private.h"
 #include "pango-emoji-private.h"
 #include "pango-attributes-private.h"
+#include "pango-item-private.h"
 
 
 /* {{{ Font cache */
@@ -1033,6 +1034,8 @@ pango_itemize_with_font (PangoContext               *context,
                          const PangoFontDescription *desc)
 {
   ItemizeState state;
+  GList *items;
+  int char_offset;
 
   if (length == 0 || g_utf8_get_char (text + start_index) == '\0')
     return NULL;
@@ -1046,7 +1049,18 @@ pango_itemize_with_font (PangoContext               *context,
 
   itemize_state_finish (&state);
 
-  return g_list_reverse (state.result);
+  items = g_list_reverse (state.result);
+
+  /* Compute the char offset for each item */
+  char_offset = 0;
+  for (GList *l = items; l; l = l->next)
+    {
+      PangoItemPrivate *item = l->data;
+      item->char_offset = char_offset;
+      char_offset += item->num_chars;
+    }
+
+  return items;
 }
 
 /**

pango/pango-item-private.h
@@ -1,0 +1,73 @@
+/* Pango
+ *
+ * Copyright (C) 2021 Matthias Clasen
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
+#ifndef __PANGO_ITEM_PRIVATE_H__
+#define __PANGO_ITEM_PRIVATE_H__
+
+#include <pango/pango-item.h>
+
+G_BEGIN_DECLS
+
+/**
+ * We have to do some extra work for adding the char_offset field
+ * to PangoItem to preserve ABI in the face of pango's open-coded
+ * structs.
+ *
+ * Internally, pango uses the PangoItemPrivate type, and we use
+ * a bit in the PangoAnalysis flags to indicate whether we are
+ * dealing with a PangoItemPrivate struct or not.
+ */
+
+#define PANGO_ANALYSIS_FLAG_HAS_CHAR_OFFSET (1 << 7)
+
+typedef struct _PangoItemPrivate PangoItemPrivate;
+
+#ifdef __x86_64__
+
+struct _PangoItemPrivate
+{
+  int offset;
+  int length;
+  int num_chars;
+  int char_offset;
+  PangoAnalysis analysis;
+};
+
+#else
+
+struct _PangoItemPrivate
+{
+  int offset;
+  int length;
+  int num_chars;
+  PangoAnalysis analysis;
+  int char_offset;
+}
+
+#endif
+
+G_STATIC_ASSERT (offsetof (PangoItem, offset) == offsetof (PangoItemPrivate, offset));
+G_STATIC_ASSERT (offsetof (PangoItem, length) == offsetof (PangoItemPrivate, length));
+G_STATIC_ASSERT (offsetof (PangoItem, num_chars) == offsetof (PangoItemPrivate, num_chars));
+G_STATIC_ASSERT (offsetof (PangoItem, analysis) == offsetof (PangoItemPrivate, analysis));
+
+G_END_DECLS
+
+#endif /* __PANGO_ITEM_PRIVATE_H__ */

pango/pango-item.c
@@ -21,7 +21,7 @@
 
 #include "config.h"
 #include "pango-attributes.h"
-#include "pango-item.h"
+#include "pango-item-private.h"
 #include "pango-impl-utils.h"
 
 /**
@@ -35,9 +35,11 @@
 PangoItem *
 pango_item_new (void)
 {
-  PangoItem *result = g_slice_new0 (PangoItem);
+  PangoItemPrivate *result = g_slice_new0 (PangoItemPrivate);
 
-  return result;
+  result->analysis.flags |= PANGO_ANALYSIS_FLAG_HAS_CHAR_OFFSET;
+
+  return (PangoItem *)result;
 }
 
 /**
@@ -57,11 +59,13 @@ pango_item_copy (PangoItem *item)
   if (item == NULL)
     return NULL;
 
-  result = g_slice_new (PangoItem);
+  result = pango_item_new ();
 
   result->offset = item->offset;
   result->length = item->length;
   result->num_chars = item->num_chars;
+  if (item->analysis.flags & PANGO_ANALYSIS_FLAG_HAS_CHAR_OFFSET)
+    ((PangoItemPrivate *)result)->char_offset = ((PangoItemPrivate *)item)->char_offset;
 
   result->analysis = item->analysis;
   if (result->analysis.font)
@@ -101,7 +105,10 @@ pango_item_free (PangoItem *item)
   if (item->analysis.font)
     g_object_unref (item->analysis.font);
 
-  g_slice_free (PangoItem, item);
+  if (item->analysis.flags & PANGO_ANALYSIS_FLAG_HAS_CHAR_OFFSET)
+    g_slice_free (PangoItemPrivate, (PangoItemPrivate *)item);
+  else
+    g_slice_free (PangoItem, item);
 }
 
 G_DEFINE_BOXED_TYPE (PangoItem, pango_item,
@@ -151,6 +158,8 @@ pango_item_split (PangoItem *orig,
   orig->offset += split_index;
   orig->length -= split_index;
   orig->num_chars -= split_offset;
+  if (orig->analysis.flags & PANGO_ANALYSIS_FLAG_HAS_CHAR_OFFSET)
+    ((PangoItemPrivate *)orig)->char_offset += split_offset;
 
   return new_item;
 }

pango/pango-item.h
@@ -100,6 +100,7 @@ struct _PangoAnalysis
  * @offset: byte offset of the start of this item in text.
  * @length: length of this item in bytes.
  * @num_chars: number of Unicode characters in the item.
+ * @char_offset: character offset of the start of this item in text. Since 1.50
  * @analysis: analysis results for the item.
  *
  * The `PangoItem` structure stores information about a segment of text.
@@ -109,9 +110,9 @@ struct _PangoAnalysis
  */
 struct _PangoItem
 {
-  gint offset;
-  gint length;
-  gint num_chars;
+  int offset;
+  int length;
+  int num_chars;
   PangoAnalysis analysis;
 };
 

commit d6527dd41616e32a224d70cd0a531940ea38b82d

Fix char offset calculations
When dealing with multi-paragraph layouts,
the char offsets of the items are expected
to be relative to the beginning of the text,
not relative to the beginning of the current
paragraph.

This error was introduced in a03bf5bc6b07ba6e.

Fixes: #716

pango/itemize.c
@@ -1495,13 +1495,14 @@ handle_variants (const char   *text,
 
 static GList *
 reorder_items (PangoContext *context,
-               GList        *items)
+               GList        *items,
+               int           initial_offset)
 {
-  int char_offset = 0;
+  int char_offset = initial_offset;
 
   items = g_list_reverse (items);
 
-  /* Also cmpute the char offset for each item here */
+  /* Also compute the char offset for each item here */
   for (GList *l = items; l; l = l->next)
     {
       PangoItemPrivate *item = l->data;
@@ -1544,6 +1545,7 @@ pango_itemize_with_font (PangoContext               *context,
                          const PangoFontDescription *desc)
 {
   ItemizeState state;
+  int initial_offset;
 
   g_return_val_if_fail (context->font_map != NULL, NULL);
 
@@ -1559,7 +1561,9 @@ pango_itemize_with_font (PangoContext               *context,
 
   itemize_state_finish (&state);
 
-  return reorder_items (context, state.result);
+  initial_offset = g_utf8_strlen (text, start_index);
+
+  return reorder_items (context, state.result, initial_offset);
 }
 
 /* Apply post-processing steps that may require log attrs.