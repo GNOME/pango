d801aaea4d10e4bd6d0319d0be1551c56a92a058
Matthias Clasen <mclasen@redhat.com>
2024-11-08 08:41:08 -0500

    tests: More Cantarell fontmap fixage
    Ensure the fontconfig case also uses the included Cantarell,
    not the system one.

--- tests/test-common.c
+++ tests/test-common.c
@@ -37,6 +37,11 @@
 #include <pango/pangowin32.h>
 #endif
 
+#ifdef HAVE_FREETYPE
+#include <pango/pangoft2.h>
+#include <pango/pangocairo-fc.h>
+#endif
+
 #include "test-common.h"
 
 
@@ -247,14 +252,57 @@ get_script_name (GUnicodeScript s)
   return nick;
 }
 
+#ifdef HAVE_FREETYPE
+static PangoFontMap *
+generate_font_map (void)
+{
+  FcConfig *config;
+  PangoFontMap *map;
+  char *path;
+  gsize len;
+  char *conf;
+  char *dir;
+
+  dir = g_test_build_filename (G_TEST_DIST, "fonts", NULL);
+
+  map = g_object_new (PANGO_TYPE_CAIRO_FC_FONT_MAP, NULL);
+
+  config = FcConfigCreate ();
+
+  path = g_build_filename (dir, "fonts.conf", NULL);
+  g_file_get_contents (path, &conf, &len, NULL);
+
+  if (!FcConfigParseAndLoadFromMemory (config, (const FcChar8 *) conf, TRUE))
+    g_error ("Failed to parse fontconfig configuration");
+
+  g_free (conf);
+  g_free (path);
+
+  FcConfigAppFontAddDir (config, (const FcChar8 *) dir);
+  pango_fc_font_map_set_config (PANGO_FC_FONT_MAP (map), config);
+  FcConfigDestroy (config);
+
+  g_free (dir);
+
+  return map;
+}
+#endif
+
+/* Create a fontmap that will return our included Cantarell-VF.otf
+ * for "Cantarell"
+ *
+ * FIXME: Make this work for macOS
+ */
 PangoFontMap *
 get_font_map_with_cantarell (void)
-{ 
+{
   PangoFontMap *fontmap;
 
+#ifdef HAVE_FREETYPE
+  fontmap = generate_font_map ();
+#elif defined (G_OS_WIN32)
   fontmap = pango_cairo_font_map_new ();
 
-#ifdef G_OS_WIN32
   if (strcmp (G_OBJECT_TYPE_NAME (fontmap), "PangoCairoWin32FontMap") == 0)
     {
       GError *error = NULL;
@@ -265,6 +313,8 @@ get_font_map_with_cantarell (void)
       g_assert_no_error (error);
       g_free (path);
     }
+#else
+  fontmap = pango_cairo_font_map_new ();
 #endif
 
   g_assert_true (pango_cairo_font_map_get_resolution (PANGO_CAIRO_FONT_MAP (fontmap)) == 96.0);
