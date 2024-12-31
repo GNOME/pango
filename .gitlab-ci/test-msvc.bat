11448c106224fb229e01e259e711ecdba927f884
Matthias Clasen <mclasen@redhat.com>
2024-12-31 18:00:43 -0500

    CI/MSVC: Add GNU diffutils in path
    We rely on diff for tests

--- .gitlab-ci/test-msvc.bat
+++ .gitlab-ci/test-msvc.bat
@@ -4,6 +4,9 @@
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Auxiliary\Build\vcvarsall.bat" x64
 @echo on
 
+:: Tests use the diff utility
+set PATH=%PATH%;C:\Program Files\Git\usr\bin
+
 pip3 install --upgrade --user meson~=1.2 || goto :error
 meson setup -Dbackend_max_links=1 -Ddebug=false _build || goto :error
 meson compile -C _build || goto :error
