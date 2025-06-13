52e1d8465a4985d41b02fbe237c205127c2c8fd3
Matthias Clasen <mclasen@redhat.com>
2025-06-13 11:27:02 -0400

    Change to using a test font
    This changes the font data test to use a synthetic test font
    where we can control all the metrics.

--- tests/test-font-data.c
+++ tests/test-font-data.c
@@ -28,50 +28,7 @@
 
 #include "test-common.h"
 
-
-static PangoFontMetrics cantarell_unhinted_metrics = {
-  .ascent = 14764,
-  .descent = 3259,
-  .height = 18023,
-  .approximate_char_width = 6734,
-  .approximate_digit_width = 9216,
-  .underline_position = -1502,
-  .underline_thickness = 751,
-  .strikethrough_position = 4340,
-  .strikethrough_thickness = 751,
-};
-
-static PangoFontMetrics cantarell_hinted_metrics = {
-  .ascent = 15360,
-  .descent = 4096,
-  .height = 18432,
-  .approximate_char_width = 6734,
-  .approximate_digit_width = 9216,
-  .underline_position = -1024,
-  .underline_thickness = 1024,
-  .strikethrough_position = 5120,
-  .strikethrough_thickness = 1024,
-};
-
-#ifdef DEBUG
-static void
-print_metrics (PangoFont *font)
-{
-  PangoFontMetrics *metrics;
-
-  metrics = pango_font_get_metrics (font, pango_language_from_string ("en"));
-
-  g_print ("ascent %d\n", metrics->ascent);
-  g_print ("descent %d\n", metrics->descent);
-  g_print ("height %d\n", metrics->height);
-  g_print ("char width %d\n", metrics->approximate_char_width);
-  g_print ("digit width %d\n", metrics->approximate_digit_width);
-  g_print ("underline %d %d\n", metrics->underline_position, metrics->underline_thickness);
-  g_print ("strikethrough %d %d\n", metrics->strikethrough_position, metrics->strikethrough_thickness);
-
-  pango_font_metrics_unref (metrics);
-}
-#endif
+#undef DEBUG
 
 static PangoFont *
 load_font_with_font_options (PangoFontMap         *fontmap,
@@ -98,62 +55,97 @@ load_font_with_font_options (PangoFontMap         *fontmap,
   return font;
 }
 
+static PangoFontMap *
+get_font_map_with_boxes (void)
+{
+  PangoFontMap *fontmap;
+  char *path;
+  GError *error = NULL;
+  PangoFontDescription *desc;
+  PangoFont *font;
+
+  fontmap = pango_cairo_font_map_new ();
+  path = g_test_build_filename (G_TEST_DIST, "fonts", "boxes.ttf", NULL);
+
+  pango_font_map_add_font_file (fontmap, path, &error);
+  g_assert_no_error (error);
+
+  desc = pango_font_description_from_string ("Boxes 1px");
+  font = load_font_with_font_options (fontmap, desc, CAIRO_HINT_STYLE_NONE, CAIRO_HINT_METRICS_OFF);
+
+  g_assert_cmpstr (pango_font_family_get_name (pango_font_face_get_family (pango_font_get_face (font))), ==,  "Boxes");
+
+  g_object_unref (font);
+  pango_font_description_free (desc);
+  g_free (path);
+
+  return fontmap;
+}
+
+static PangoFontMetrics boxes_unhinted_metrics = {
+  .ascent = 2048,
+  .descent = 1024,
+  .height = 3072,
+  .approximate_char_width = 590,
+  .approximate_digit_width = 1024,
+  .underline_position = -1024,
+  .underline_thickness = 1024,
+  .strikethrough_position = 1024,
+  .strikethrough_thickness = 1024,
+};
+
+#ifdef DEBUG
 static void
-test_cantarell_font_metrics (void)
+print_metrics (PangoFont *font)
+{
+  PangoFontMetrics *metrics;
+
+  metrics = pango_font_get_metrics (font, pango_language_from_string ("en"));
+
+  g_print ("ascent %d\n", metrics->ascent);
+  g_print ("descent %d\n", metrics->descent);
+  g_print ("height %d\n", metrics->height);
+  g_print ("char width %d\n", metrics->approximate_char_width);
+  g_print ("digit width %d\n", metrics->approximate_digit_width);
+  g_print ("underline %d %d\n", metrics->underline_position, metrics->underline_thickness);
+  g_print ("strikethrough %d %d\n", metrics->strikethrough_position, metrics->strikethrough_thickness);
+
+  pango_font_metrics_unref (metrics);
+}
+#endif
+
+static void
+test_boxes_font_metrics (void)
 {
   PangoFontMap *fontmap;
   PangoFontDescription *desc;
   PangoFont *font;
   PangoFontMetrics *metrics;
 
-  desc = pango_font_description_from_string ("Cantarell 11");
-  fontmap = get_font_map_with_cantarell ();
+  fontmap = get_font_map_with_boxes ();
+
+  desc = pango_font_description_from_string ("Boxes 1px");
   font = load_font_with_font_options (fontmap, desc, CAIRO_HINT_STYLE_NONE, CAIRO_HINT_METRICS_OFF);
 
-  if (strcmp (pango_font_family_get_name (pango_font_face_get_family (pango_font_get_face (font))), "Cantarell") != 0)
-    {
-      g_test_skip ("Fontmap has no Cantarell");
-      return;
-    }
-
 #ifdef DEBUG
   print_metrics (font);
+  return;
 #endif
 
   metrics = pango_font_get_metrics (font, pango_language_from_string ("en"));
 
-  g_assert_cmpint (metrics->ascent, ==, cantarell_unhinted_metrics.ascent);
-  g_assert_cmpint (metrics->descent, ==, cantarell_unhinted_metrics.descent);
-  g_assert_cmpint (metrics->approximate_char_width, ==, cantarell_unhinted_metrics.approximate_char_width);
-  g_assert_cmpint (metrics->approximate_digit_width, ==, cantarell_unhinted_metrics.approximate_digit_width);
-  g_assert_cmpint (metrics->underline_position, ==, cantarell_unhinted_metrics.underline_position);
-  g_assert_cmpint (metrics->underline_thickness, ==, cantarell_unhinted_metrics.underline_thickness);
-  g_assert_cmpint (metrics->strikethrough_position, ==, cantarell_unhinted_metrics.strikethrough_position);
-  g_assert_cmpint (metrics->strikethrough_thickness, ==, cantarell_unhinted_metrics.strikethrough_thickness);
+  g_assert_cmpint (metrics->ascent, ==, boxes_unhinted_metrics.ascent);
+  g_assert_cmpint (metrics->descent, ==, boxes_unhinted_metrics.descent);
+  g_assert_cmpint (metrics->approximate_char_width, ==, boxes_unhinted_metrics.approximate_char_width);
+  g_assert_cmpint (metrics->approximate_digit_width, ==, boxes_unhinted_metrics.approximate_digit_width);
+  g_assert_cmpint (metrics->underline_position, ==, boxes_unhinted_metrics.underline_position);
+  g_assert_cmpint (metrics->underline_thickness, ==, boxes_unhinted_metrics.underline_thickness);
+  g_assert_cmpint (metrics->strikethrough_position, ==, boxes_unhinted_metrics.strikethrough_position);
+  g_assert_cmpint (metrics->strikethrough_thickness, ==, boxes_unhinted_metrics.strikethrough_thickness);
 
   pango_font_metrics_unref (metrics);
+
   g_object_unref (font);
-
-  font = load_font_with_font_options (fontmap, desc, CAIRO_HINT_STYLE_FULL, CAIRO_HINT_METRICS_ON);
-
-#ifdef DEBUG
-  print_metrics (font);
-#endif
-
-  metrics = pango_font_get_metrics (font, pango_language_from_string ("en"));
-
-  g_assert_cmpint (metrics->ascent, ==, cantarell_hinted_metrics.ascent);
-  g_assert_cmpint (metrics->descent, ==, cantarell_hinted_metrics.descent);
-  g_assert_cmpint (metrics->approximate_char_width, ==, cantarell_hinted_metrics.approximate_char_width);
-  g_assert_cmpint (metrics->approximate_digit_width, ==, cantarell_hinted_metrics.approximate_digit_width);
-  g_assert_cmpint (metrics->underline_position, ==, cantarell_hinted_metrics.underline_position);
-  g_assert_cmpint (metrics->underline_thickness, ==, cantarell_hinted_metrics.underline_thickness);
-  g_assert_cmpint (metrics->strikethrough_position, ==, cantarell_hinted_metrics.strikethrough_position);
-  g_assert_cmpint (metrics->strikethrough_thickness, ==, cantarell_hinted_metrics.strikethrough_thickness);
-
-  pango_font_metrics_unref (metrics);
-  g_object_unref (font);
-
   pango_font_description_free (desc);
   g_object_unref (fontmap);
 }
@@ -166,214 +158,78 @@ typedef struct
   PangoRectangle logical;
 } GlyphMetrics;
 
-static GlyphMetrics cantarell_unhinted_glyphs[] = {
-  { .name = "a",
-    .id = 244,
-    .ink = (PangoRectangle) { 706, -7389, 5857, 7540 },
-    .logical = (PangoRectangle) { 0, -14764, 7690, 18023 }
-  },
-  { .name = "b",
-    .id = 272,
-    .ink = (PangoRectangle) { 1247, -11099, 6533, 11249 },
-    .logical = (PangoRectangle) { 0, -14764, 8561, 18023 }
-  },
-  { .name = "c",
-    .id = 273,
-    .ink = (PangoRectangle) { 781, -7389, 5677, 7540 },
-    .logical = (PangoRectangle) { 0, -14764, 7014, 18023 }
-  },
-  { .name = "d",
-    .id = 280,
-    .ink = (PangoRectangle) { 781, -11099, 6533, 11249 },
-    .logical = (PangoRectangle) { 0, -14764, 8561, 18023 }
-  },
-  { .name = "e",
-    .id = 287,
-    .ink = (PangoRectangle) { 781, -7389, 6368, 7540 },
-    .logical = (PangoRectangle) { 0, -14764, 7930, 18023 }
-  },
-  { .name = "f",
-    .id = 311,
-    .ink = (PangoRectangle) { 406, -11249, 5076, 11249 },
-    .logical = (PangoRectangle) { 0, -14764, 5106, 18023 }
-  },
-  { .name = "g",
-    .id = 312,
-    .ink = (PangoRectangle) { 781, -7389, 6533, 10799 },
-    .logical = (PangoRectangle) { 0, -14764, 8561, 18023 }
-  },
-  { .name = "h",
-    .id = 319,
-    .ink = (PangoRectangle) { 1247, -11099, 6143, 11099 },
-    .logical = (PangoRectangle) { 0, -14764, 8516, 18023 }
-  },
+static GlyphMetrics boxes_unhinted_glyphs[] = {
   { .name = "A",
     .id = 1,
-    .ink = (PangoRectangle) { 105, -10423, 9192, 10423 },
-    .logical = (PangoRectangle) { 0, -14764, 9402, 18023 }
+    .ink = (PangoRectangle) { 0, -1, 1, 1 },
+    .logical = (PangoRectangle) { 0, -2048, 1024, 3072 }
   },
   { .name = "B",
-    .id = 29,
-    .ink = (PangoRectangle) { 1382, -10423, 7449, 10423 },
-    .logical = (PangoRectangle) { 0, -14764, 9462, 18023 }
+    .id = 2,
+    .ink = (PangoRectangle) { 0, -2, 2, 2 },
+    .logical = (PangoRectangle) { 0, -2048, 1024, 3072 }
   },
   { .name = "C",
-    .id = 30,
-    .ink = (PangoRectangle) { 811, -10558, 8245, 10709 },
-    .logical = (PangoRectangle) { 0, -14764, 9687, 18023 }
+    .id = 3,
+    .ink = (PangoRectangle) { 0, -4, 4, 4 },
+    .logical = (PangoRectangle) { 0, -2048, 1024, 3072 }
   },
   { .name = "D",
-    .id = 37,
-    .ink = (PangoRectangle) { 1382, -10423, 8486, 10423 },
-    .logical = (PangoRectangle) { 0, -14764, 10679, 18023 }
+    .id = 4,
+    .ink = (PangoRectangle) { 0, -8, 8, 8 },
+    .logical = (PangoRectangle) { 0, -2048, 1024, 3072 }
   },
   { .name = "E",
-    .id = 45,
-    .ink = (PangoRectangle) { 1382, -10423, 6548, 10423 },
-    .logical = (PangoRectangle) { 0, -14764, 8771, 18023 }
+    .id = 5,
+    .ink = (PangoRectangle) { 0, -16, 16, 16 },
+    .logical = (PangoRectangle) { 0, -2048, 1024, 3072 }
   },
   { .name = "F",
-    .id = 68,
-    .ink = (PangoRectangle) { 1382, -10423, 6473, 10423 },
-    .logical = (PangoRectangle) { 0, -14764, 8456, 18023 }
+    .id = 6,
+    .ink = (PangoRectangle) { 0, -32, 32, 32 },
+    .logical = (PangoRectangle) { 0, -2048, 1024, 3072 }
   },
   { .name = "G",
-    .id = 69,
-    .ink = (PangoRectangle) { 811, -10558, 9026, 10694 },
-    .logical = (PangoRectangle) { 0, -14764, 10739, 18023 }
+    .id = 7,
+    .ink = (PangoRectangle) { 0, -64, 64, 64 },
+    .logical = (PangoRectangle) { 0, -2048, 1024, 3072 }
   },
   { .name = "H",
-    .id = 76,
-    .ink = (PangoRectangle) { 1382, -10423, 8065, 10423 },
-    .logical = (PangoRectangle) { 0, -14764, 10829, 18023 }
-  }
-};
-
-static GlyphMetrics cantarell_hinted_glyphs[] = {
-  { .name = "a",
-    .id = 244,
-    .ink = (PangoRectangle) { 0, -8192, 7168, 8192 },
-    .logical = (PangoRectangle) { 0, -15360, 8192, 19456 }
+    .id = 8,
+    .ink = (PangoRectangle) { 0, -128, 128, 128 },
+    .logical = (PangoRectangle) { 0, -2048, 1024, 3072 }
   },
-  { .name = "b",
-    .id = 272,
-    .ink = (PangoRectangle) { 1024, -11264, 7168, 11264 },
-    .logical = (PangoRectangle) { 0, -15360, 8192, 19456 }
+  { .name = "I",
+    .id = 9,
+    .ink = (PangoRectangle) { 0, -256, 256, 256 },
+    .logical = (PangoRectangle) { 0, -2048, 1024, 3072 }
   },
-  { .name = "c",
-    .id = 273,
-    .ink = (PangoRectangle) { 0, -8192, 7168, 8192 },
-    .logical = (PangoRectangle) { 0, -15360, 7168, 19456 }
+  { .name = "J",
+    .id = 10,
+    .ink = (PangoRectangle) { 0, -512, 512, 512 },
+    .logical = (PangoRectangle) { 0, -2048, 1024, 3072 }
   },
-  { .name = "d",
-    .id = 280,
-    .ink = (PangoRectangle) { 0, -11264, 8192, 11264 },
-    .logical = (PangoRectangle) { 0, -15360, 8192, 19456 }
-  },
-  { .name = "e",
-    .id = 287,
-    .ink = (PangoRectangle) { 0, -8192, 7168, 8192 },
-    .logical = (PangoRectangle) { 0, -15360, 8192, 19456 }
-  },
-  { .name = "f",
-    .id = 311,
-    .ink = (PangoRectangle) { 0, -11264, 6144, 11264 },
-    .logical = (PangoRectangle) { 0, -15360, 5120, 19456 }
-  },
-  { .name = "g",
-    .id = 312,
-    .ink = (PangoRectangle) { 0, -8192, 8192, 12288 },
-    .logical = (PangoRectangle) { 0, -15360, 8192, 19456 }
-  },
-  { .name = "h",
-    .id = 319,
-    .ink = (PangoRectangle) { 1024, -11264, 7168, 11264 },
-    .logical = (PangoRectangle) { 0, -15360, 8192, 19456 }
-  },
-  { .name = "n",
-    .id = 360,
-    .ink = (PangoRectangle) { 1024, -8192, 7168, 8192 },
-    .logical = (PangoRectangle) { 0, -15360, 8192, 19456 }
-  },
-  { .name = "t",
-    .id = 430,
-    .ink = (PangoRectangle) { 0, -11264, 5120, 11264 },
-    .logical = (PangoRectangle) { 0, -15360, 5120, 19456 }
-  },
-  { .name = "p",
-    .id = 406,
-    .ink = (PangoRectangle) { 1024, -8192, 7168, 12288 },
-    .logical = (PangoRectangle) { 0, -15360, 8192, 19456 }
-  },
-
-  { .name = "A",
-    .id = 1,
-    .ink = (PangoRectangle) { 0, -11264, 10240, 11264 },
-    .logical = (PangoRectangle) { 0, -15360, 9216, 19456 }
-  },
-  { .name = "B",
-    .id = 29,
-    .ink = (PangoRectangle) { 1024, -11264, 8192, 11264 },
-    .logical = (PangoRectangle) { 0, -15360, 9216, 19456 }
-  },
-  { .name = "C",
-    .id = 30,
-    .ink = (PangoRectangle) { 0, -11264, 9216, 11264 },
-    .logical = (PangoRectangle) { 0, -15360, 9216, 19456 }
-  },
-  { .name = "D",
-    .id = 37,
-    .ink = (PangoRectangle) { 1024, -11264, 9216, 11264 },
-    .logical = (PangoRectangle) { 0, -15360, 10240, 19456 }
-  },
-  { .name = "E",
-    .id = 45,
-    .ink = (PangoRectangle) { 1024, -11264, 7168, 11264 },
-    .logical = (PangoRectangle) { 0, -15360, 9216, 19456 }
-  },
-  { .name = "F",
-    .id = 68,
-    .ink = (PangoRectangle) { 1024, -11264, 7168, 11264 },
-    .logical = (PangoRectangle) { 0, -15360, 8192, 19456 }
-  },
-  { .name = "G",
-    .id = 69,
-    .ink = (PangoRectangle) { 0, -11264, 10240, 11264 },
-    .logical = (PangoRectangle) { 0, -15360, 10240, 19456 }
-  },
-  { .name = "H",
-    .id = 76,
-    .ink = (PangoRectangle) { 1024, -11264, 9216, 11264 },
-    .logical = (PangoRectangle) { 0, -15360, 11264, 19456 }
-  },
-  { .name = "N",
-    .id = 115,
-    .ink = (PangoRectangle) { 1024, -11264, 9216, 11264 },
-    .logical = (PangoRectangle) { 0, -15360, 11264, 19456 }
-  },
-  { .name = "T",
-    .id = 187,
-    .ink = (PangoRectangle) { 0, -11264, 9216, 11264 },
-    .logical = (PangoRectangle) { 0, -15360, 8192, 19456 }
-  },
-  { .name = "P",
-    .id = 162,
-    .ink = (PangoRectangle) { 1024, -11264, 8192, 11264 },
-    .logical = (PangoRectangle) { 0, -15360, 9216, 19456 }
+  { .name = "K",
+    .id = 11,
+    .ink = (PangoRectangle) { 0, -1024, 1024, 1024 },
+    .logical = (PangoRectangle) { 0, -2048, 1024, 3072 }
   },
 };
 
 #ifdef DEBUG
 static void
 print_glyph_metrics (PangoFont  *font,
-                     const char *name)
+                     gunichar    ch)
 {
   hb_font_t *hb_font;
   hb_codepoint_t glyph;
+  char name[6];
+
+  g_unichar_to_utf8 (ch, name);
 
   hb_font = pango_font_get_hb_font (font);
 
-  if (!hb_font_get_glyph_from_name (hb_font, name, -1, &glyph))
+  if (!hb_font_get_glyph (hb_font, ch, 0, &glyph))
     {
       g_print ("Not a glyph: %s\n", name);
     }
@@ -403,65 +259,35 @@ pango_rectangle_equal (const PangoRectangle *a,
 }
 
 static void
-test_cantarell_glyph_metrics (void)
+test_boxes_glyph_metrics (void)
 {
   PangoFontDescription *desc;
   PangoFontMap *fontmap;
   PangoFont *font;
 
-  desc = pango_font_description_from_string ("Cantarell 11");
-  fontmap = get_font_map_with_cantarell ();
+  fontmap = get_font_map_with_boxes ();
+
+  desc = pango_font_description_from_string ("Boxes 1px");
   font = load_font_with_font_options (fontmap, desc, CAIRO_HINT_STYLE_NONE, CAIRO_HINT_METRICS_OFF);
 
-  if (strcmp (pango_font_family_get_name (pango_font_face_get_family (pango_font_get_face (font))), "Cantarell") != 0)
-    {
-      g_test_skip ("Fontmap has no Cantarell");
-      return;
-    }
-
-  for (int i = 0; i < G_N_ELEMENTS (cantarell_unhinted_glyphs); i++)
-    {
-      GlyphMetrics *gm = &cantarell_unhinted_glyphs[i];
-      PangoRectangle ink, logical;
-
-      pango_font_get_glyph_extents (font, gm->id, &ink, &logical);
-      g_assert_true (pango_rectangle_equal (&ink, &gm->ink));
-      g_assert_true (pango_rectangle_equal (&logical, &gm->logical));
-    }
-
-  g_object_unref (font);
-
-  font = load_font_with_font_options (fontmap, desc, CAIRO_HINT_STYLE_FULL, CAIRO_HINT_METRICS_ON);
-
 #ifdef DEBUG
-  print_glyph_metrics (font, "a");
-  print_glyph_metrics (font, "b");
-  print_glyph_metrics (font, "c");
-  print_glyph_metrics (font, "d");
-  print_glyph_metrics (font, "e");
-  print_glyph_metrics (font, "f");
-  print_glyph_metrics (font, "g");
-  print_glyph_metrics (font, "h");
-  print_glyph_metrics (font, "n");
-  print_glyph_metrics (font, "t");
-  print_glyph_metrics (font, "p");
-
-  print_glyph_metrics (font, "A");
-  print_glyph_metrics (font, "B");
-  print_glyph_metrics (font, "C");
-  print_glyph_metrics (font, "D");
-  print_glyph_metrics (font, "E");
-  print_glyph_metrics (font, "F");
-  print_glyph_metrics (font, "G");
-  print_glyph_metrics (font, "H");
-  print_glyph_metrics (font, "N");
-  print_glyph_metrics (font, "T");
-  print_glyph_metrics (font, "P");
+  print_glyph_metrics (font, 'A');
+  print_glyph_metrics (font, 'B');
+  print_glyph_metrics (font, 'C');
+  print_glyph_metrics (font, 'D');
+  print_glyph_metrics (font, 'E');
+  print_glyph_metrics (font, 'F');
+  print_glyph_metrics (font, 'G');
+  print_glyph_metrics (font, 'H');
+  print_glyph_metrics (font, 'I');
+  print_glyph_metrics (font, 'J');
+  print_glyph_metrics (font, 'K');
+  return;
 #endif
 
-  for (int i = 0; i < G_N_ELEMENTS (cantarell_hinted_glyphs); i++)
+  for (int i = 0; i < G_N_ELEMENTS (boxes_unhinted_glyphs); i++)
     {
-      GlyphMetrics *gm = &cantarell_hinted_glyphs[i];
+      GlyphMetrics *gm = &boxes_unhinted_glyphs[i];
       PangoRectangle ink, logical;
 
       pango_font_get_glyph_extents (font, gm->id, &ink, &logical);
@@ -470,7 +296,6 @@ test_cantarell_glyph_metrics (void)
     }
 
   g_object_unref (font);
-
   pango_font_description_free (desc);
   g_object_unref (fontmap);
 }
@@ -483,8 +308,9 @@ main (int argc, char *argv[])
   g_test_init (&argc, &argv, NULL);
   g_test_set_nonfatal_assertions ();
 
-  g_test_add_func ("/pango/font/cantarell/font-metrics", test_cantarell_font_metrics);
-  g_test_add_func ("/pango/font/cantarell/glyph-metrics", test_cantarell_glyph_metrics);
+  g_test_add_func ("/pango/font/boxes/font-metrics", test_boxes_font_metrics);
+  g_test_add_func ("/pango/font/boxes/glyph-metrics", test_boxes_glyph_metrics);
+
 
   return g_test_run ();
 }
