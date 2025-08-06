GNOME/pango

commit b51d25d71f05b4470c989fa4bf993d9083dd8846

Add color to font descriptions
This allows to match fonts based on whether the do or do not
have color glyphs. This will be used to improve Emoji font
selection.

pango/fonts.c
@@ -41,6 +41,7 @@ struct _PangoFontDescription
   PangoWeight weight;
   PangoStretch stretch;
   PangoGravity gravity;
+  PangoFontColor color;
 
   char *variations;
   char *features;
@@ -66,6 +67,7 @@ static const PangoFontDescription pfd_defaults = {
   PANGO_WEIGHT_NORMAL,  /* weight */
   PANGO_STRETCH_NORMAL, /* stretch */
   PANGO_GRAVITY_SOUTH,  /* gravity */
+  PANGO_FONT_COLOR_DONT_CARE,  /* color */
   NULL,                 /* variations */
   NULL,                 /* features */
 
@@ -713,6 +715,51 @@ pango_font_description_get_features (const PangoFontDescription *desc)
 }
 
 /**
+ * pango_font_description_set_color:
+ * @desc: a `PangoFontDescription`.
+ * @color: the `PangoFontColor` value
+ *
+ * Sets the color field of a font description.
+ *
+ * This field determines whether the font description should
+ * match fonts that have color glyphs, or fonts that don't.
+ *
+ * Since: 1.57
+ */
+void
+pango_font_description_set_color (PangoFontDescription *desc,
+                                  PangoFontColor        color)
+
+{
+  g_return_if_fail (desc != NULL);
+
+  if (desc->color == color)
+    return;
+
+  desc->color = color;
+  desc->mask |= PANGO_FONT_MASK_COLOR;
+}
+
+/**
+ * pango_font_description_get_color:
+ * @desc: a `PangoFontDescription`
+ *
+ * Returns the color field of the font description.
+ *
+ * This field determines whether the font description should
+ * match fonts that have color glyphs, or fonts that don't.
+ *
+ * Since: 1.57
+ */
+PangoFontColor
+pango_font_description_get_color (const PangoFontDescription *desc)
+{
+  g_return_val_if_fail (desc != NULL, PANGO_FONT_COLOR_DONT_CARE);
+
+  return desc->color;
+}
+
+/**
  * pango_font_description_get_set_fields:
  * @desc: a `PangoFontDescription`
  *
@@ -859,6 +906,8 @@ pango_font_description_merge_static (PangoFontDescription       *desc,
     }
   if (new_mask & PANGO_FONT_MASK_GRAVITY)
     desc->gravity = desc_to_merge->gravity;
+  if (new_mask & PANGO_FONT_MASK_COLOR)
+    desc->gravity = desc_to_merge->color;
   if (new_mask & PANGO_FONT_MASK_VARIATIONS)
     pango_font_description_set_variations_static (desc, desc_to_merge->variations);
   if (new_mask & PANGO_FONT_MASK_FEATURES)
@@ -916,7 +965,9 @@ pango_font_description_better_match (const PangoFontDescription *desc,
 
   if (new_match->variant == desc->variant &&
       new_match->stretch == desc->stretch &&
-      new_match->gravity == desc->gravity)
+      new_match->gravity == desc->gravity &&
+      (desc->color == PANGO_FONT_COLOR_DONT_CARE ||
+       new_match->color == desc->color))
     {
       int old_distance = old_match ? compute_distance (desc, old_match) : G_MAXINT;
       int new_distance = compute_distance (desc, new_match);
@@ -1034,6 +1085,7 @@ pango_font_description_equal (const PangoFontDescription *desc1,
          desc1->size == desc2->size &&
          desc1->size_is_absolute == desc2->size_is_absolute &&
          desc1->gravity == desc2->gravity &&
+         desc1->color == desc2->color &&
          (desc1->family_name == desc2->family_name ||
           (desc1->family_name && desc2->family_name && g_ascii_strcasecmp (desc1->family_name, desc2->family_name) == 0)) &&
          (g_strcmp0 (desc1->variations, desc2->variations) == 0) &&
@@ -1089,6 +1141,7 @@ pango_font_description_hash (const PangoFontDescription *desc)
   hash ^= desc->weight << 16;
   hash ^= desc->stretch << 26;
   hash ^= desc->gravity << 28;
+  hash ^= desc->color << 29;
 
   return hash;
 }
@@ -1211,6 +1264,11 @@ static const FieldMap gravity_map[] = {
   { PANGO_GRAVITY_WEST,  "West" }
 };
 
+static const FieldMap color_map[] = {
+  { PANGO_FONT_COLOR_REQUIRED, "With-Color" },
+  { PANGO_FONT_COLOR_FORBIDDEN, "Without-Color" },
+};
+
 static gboolean
 field_matches (const gchar *s1,
                const gchar *s2,
@@ -1319,6 +1377,7 @@ find_field_any (const char *str, int len, PangoFontDescription *desc)
   FIELD (stretch, PANGO_FONT_MASK_STRETCH);
   FIELD (variant, PANGO_FONT_MASK_VARIANT);
   FIELD (gravity, PANGO_FONT_MASK_GRAVITY);
+  FIELD (color,   PANGO_FONT_MASK_COLOR);
 
 #undef FIELD
 
@@ -1436,6 +1495,9 @@ parse_features (const char  *word,
  * The following words are understood as gravity values:
  * "Not-Rotated", "South", "Upside-Down", "North", "Rotated-Left",
  * "East", "Rotated-Right", "West".
+ *
+ * The following words are understood as color values:
+ * "With-Color", "Without-Color".
  *
  * VARIATIONS is a comma-separated list of font variations
  * of the form @‍axis1=value,axis2=value,...
@@ -1766,6 +1828,8 @@ pango_font_description_to_string (const PangoFontDescription *desc)
   FIELD (variant, PANGO_FONT_MASK_VARIANT);
   if (desc->mask & PANGO_FONT_MASK_GRAVITY)
     FIELD (gravity, PANGO_FONT_MASK_GRAVITY);
+  if (desc->mask & PANGO_FONT_MASK_COLOR)
+    FIELD (color, PANGO_FONT_MASK_COLOR);
 
 #undef FIELD
 

pango/pango-font.h
@@ -202,6 +202,13 @@ typedef enum {
  *
  * Since: 1.56
  */
+/**
+ * PANGO_FONT_MASK_COLOR:
+ *
+ * Font color is specified.
+ *
+ * Since: 1.57
+ */
 typedef enum {
   PANGO_FONT_MASK_FAMILY  = 1 << 0,
   PANGO_FONT_MASK_STYLE   = 1 << 1,
@@ -212,6 +219,7 @@ typedef enum {
   PANGO_FONT_MASK_GRAVITY = 1 << 6,
   PANGO_FONT_MASK_VARIATIONS = 1 << 7,
   PANGO_FONT_MASK_FEATURES = 1 << 8,
+  PANGO_FONT_MASK_COLOR    = 1 << 9,
 } PangoFontMask;
 
 /* CSS scale factors (1.2 factor between each size) */
@@ -257,6 +265,22 @@ typedef enum {
 #define PANGO_SCALE_LARGE    ((double)1.2)
 #define PANGO_SCALE_X_LARGE  ((double)1.44)
 #define PANGO_SCALE_XX_LARGE ((double)1.728)
+
+/**
+ * PangoFontColor:
+ * @PANGO_FONT_COLOR_FORBIDDEN: The font should not have color glyphs
+ * @PANGO_FONT_COLOR_REQUIRED: The font should have color glyphs
+ * @PANGO_FONT_COLOR_DONT_CARE: The font may or may not use color
+ *
+ * Specifies whether a font should or should not have color glyphs.
+ *
+ * Since: 1.57
+ */
+typedef enum {
+  PANGO_FONT_COLOR_FORBIDDEN,
+  PANGO_FONT_COLOR_REQUIRED,
+  PANGO_FONT_COLOR_DONT_CARE,
+} PangoFontColor;
 
 /*
  * PangoFontDescription
@@ -344,6 +368,12 @@ void                 pango_font_description_set_features        (PangoFontDescri
                                                                  const char                 *features);
 PANGO_AVAILABLE_IN_1_42
 const char          *pango_font_description_get_features        (const PangoFontDescription *desc) G_GNUC_PURE;
+
+PANGO_AVAILABLE_IN_1_57
+void                 pango_font_description_set_color           (PangoFontDescription       *desc,
+                                                                 PangoFontColor              color);
+PANGO_AVAILABLE_IN_1_57
+PangoFontColor       pango_font_description_get_color           (const PangoFontDescription *desc);
 
 PANGO_AVAILABLE_IN_ALL
 PangoFontMask pango_font_description_get_set_fields (const PangoFontDescription *desc) G_GNUC_PURE;

commit e2cccfc305b913411bbb51eb0485041a246b3f15

Fix -Wenum-conversion warning with PangoFontColor
Fixes: b51d25d71f

pango/fonts.c
@@ -907,7 +907,7 @@ pango_font_description_merge_static (PangoFontDescription       *desc,
   if (new_mask & PANGO_FONT_MASK_GRAVITY)
     desc->gravity = desc_to_merge->gravity;
   if (new_mask & PANGO_FONT_MASK_COLOR)
-    desc->gravity = desc_to_merge->color;
+    desc->color = desc_to_merge->color;
   if (new_mask & PANGO_FONT_MASK_VARIATIONS)
     pango_font_description_set_variations_static (desc, desc_to_merge->variations);
   if (new_mask & PANGO_FONT_MASK_FEATURES)